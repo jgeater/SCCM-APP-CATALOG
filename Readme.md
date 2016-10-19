bff79ff180b48692b453206520a4ed7366ecc295
James Geater <jgeater@outlook.com>
2016-10-19 10:40:11 -0400

    initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+SCCM Application catalog
+
+This is a C# App that just queries WMI (locally) for the URL of your SCCM application catalog and the launched IE 11 with that URL.
+
---
+++ SCCM Application Catalog/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+using System.Windows.Forms;
+using System.Management.Instrumentation;
+
+using System.Diagnostics;
+
+
+namespace Allstate_SW_Catalog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try// Get the client's SMS_ClientSDK class.
+            {
+                ManagementScope s = new ManagementScope("root\\ccm\\clientsdk");
+                ManagementPath p = new ManagementPath("CCM_SoftwareCatalogUtilities");
+                ManagementClass cls = new ManagementClass(s, p, null);
+
+                // get the catalog URL.
+                ManagementBaseObject outSiteParams = cls.InvokeMethod("GetPortalUrlValue", null, null);
+
+                Process pr = new Process();
+
+                pr.StartInfo.FileName = "c:\\Program Files\\Internet Explorer\\iexplore.exe";
+                pr.StartInfo.Arguments = (outSiteParams["PortalUrl"].ToString());
+                pr.Start();
+
+            }
+            catch (ManagementException e)
+            {
+                Console.WriteLine("Failed to execute method", e);
+                MessageBox.Show("Error! Could not find SCCM Store URL!", "Error");
+            }
+        }
+    }
+}
